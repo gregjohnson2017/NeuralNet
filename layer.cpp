a2a39b541997fc86c0cf7ad34bde0aa75b97f346
Greg <gregjohnson2017@gmail.com>
2017-09-01 00:28:26 -0500

    Add constructor to layer.cpp

--- layer.cpp
+++ layer.cpp
@@ -1,12 +1,14 @@
+#include "neuron.h"
+
 class Layer{
 	public:
 		Neuron neurons[];
 		int nNeurons;
-		Layer(int nNeurons){
+		Layer(int nNeurons, int nInputs){
 			this->nNeurons = nNeurons;
 			neurons = new Neuron[nNeurons];
 			for(int i = 0; i < nNeurons; i++){
-				neurons[i] = new Neuron();
+				neurons[i] = new Neuron(nInputs);
 			}
 		}
 }
