09c3190d91c9825be8d76a148f5f34d9253fbd70
greg <gregjohnson2017@gmail.com>
2017-09-27 15:57:30 -0500

    Add distinct test and train main functions

---
+++ main_test.cpp
@@ -0,0 +1,27 @@
+#include "utils.h"
+#include "neuron.h"
+#include "layer.h"
+#include "network.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <vector>
+
+extern "C" {
+#include "./Quantifier/quantify.h"
+}
+
+using namespace std;
+
+int main(int argc, char **argv){
+  Network *n = new Network("network.nn", NULL);
+  const char *emnist_testing = "./Quantifier/NetworkData/emnist-digits-testing.dat";
+  //const char *mnist_testing = "./Quantifier/NetworkData/mnist-digits-testing.dat";
+  //const char *mynums = "./Quantifier/NetworkData/dadnums.dat";
+  //n->test(mnist_testing);
+  n->test(emnist_testing);
+  //n->test(mynums);
+  return 1;
+}
+
---
+++ main_train.cpp
@@ -0,0 +1,25 @@
+#include "utils.h"
+#include "neuron.h"
+#include "layer.h"
+#include "network.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <vector>
+
+extern "C" {
+#include "./Quantifier/quantify.h"
+}
+
+using namespace std;
+
+int main(int argc, char **argv){
+  srand(time(NULL));
+  Network *n = new Network(4, 28*28, 26, NULL);
+  n->train("./Quantifier/NetworkData/emnist-letters -training.dat", 1.0);
+  n->save("networkL.nn");
+  return 1;
+}
+
+
